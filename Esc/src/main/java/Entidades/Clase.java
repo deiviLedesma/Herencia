79a0da229eded46bb9b68e1eaa34a1c20999702d
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2025-03-23 13:31:55 -0700

    Add files via upload

---
+++ Esc/src/main/java/Entidades/Clase.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidades;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Clase implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String codigo;
+    private String nombre;
+
+    @ManyToOne
+    private Profesor profesor;
+
+    @ManyToMany
+    @JoinTable(name = "clase_estudiante", joinColumns = @JoinColumn(name = "clase_id"),
+            inverseJoinColumns = @JoinColumn(name = "estudiante_id"))
+    private List<Estudiante> estudiantes;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public List<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(List<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
+    }
+
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Clase)) {
+            return false;
+        }
+        Clase other = (Clase) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entidades.Clase[ id=" + id + " ]";
+    }
+
+}
---
+++ Esc/src/main/java/com/mycompany/esc/Esc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.esc;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class Esc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
